ab4470739f29c99c173acb96c8cb74a92e0ffd84
Brayan Melo <brayan.acevedo28@gmail.com>
2026-03-18 17:06:05 -0500

    init.py

--- features/actas_medicamentos/generador_actas/core/__init__.py
+++ features/actas_medicamentos/generador_actas/core/__init__.py
@@ -1,1 +1,8 @@
-from generador_actas_service import *
+from .generador_actas_service import cargar_datos, generar_todos_los_pdfs, cargar_imagen_firma, limpiar_archivos_temporales
+
+__all__ = [
+    "cargar_datos",
+    "generar_todos_los_pdfs",
+    "cargar_imagen_firma",
+    "limpiar_archivos_temporales",
+]
